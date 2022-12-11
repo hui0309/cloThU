9854cd9eff6554dbedcb6a9aea775f5d89f3e9ce
hui0309 <abbyhuang20020309@gmail.com>
2022-12-12 02:47:05 +0800

    'user_schemale'

---
+++ user.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2022-12-11 19:46:23
+-- 伺服器版本： 10.4.25-MariaDB
+-- PHP 版本： 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `clothu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` varchar(20) NOT NULL,
+  `user_name` varchar(20) NOT NULL,
+  `user_img` varchar(1024) DEFAULT NULL,
+  `user_pass` varchar(20) NOT NULL,
+  `user_mail` varchar(1024) NOT NULL,
+  `user_mail_verify` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 傾印資料表的資料 `user`
+--
+
+INSERT INTO `user` (`user_id`, `user_name`, `user_img`, `user_pass`, `user_mail`, `user_mail_verify`) VALUES
+('00957117', '00957117', NULL, '$2y$10$oRyBxyiTDgr/e', '00957117@email.ntou.edu.tw', 0);
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
