8207ef050dd409e45c972df2cd5046ebcd9294b5
huahua1018 <shelly10182433@gmail.com>
2023-01-04 13:33:12 +0800

    effect

--- index.php
+++ index.php
@@ -1,34 +1,107 @@
 <?php
-    // Check if the user is already logged in, if yes then redirect him to welcome page
-    include("session_init.php");
-    // Processing form data when form is submitted
-    if(isset($_SESSION["user_id"])){
-        header('Location: ./person.php');
-        exit;
-    }
+// Check if the user is already logged in, if yes then redirect him to welcome page
+include("session_init.php");
+// Processing form data when form is submitted
+if (isset($_SESSION["user_id"])) {
+    header('Location: ./person.php');
+    exit;
+}
 ?>
 <html>
 <!--<meta http-equiv="Content-Type" content="text/html; charset=utf-8">-->
-<script >
+<script>
     var now = "login";
     var src = "./login.html";
-    function change(login){
-        now = (login==1? "login" : "register");
-        src = (login==1? "./login.html" : "./register.html");
+    function change(login) {
+        now = (login == 1 ? "login" : "register");
+        src = (login == 1 ? "./login.html" : "./register.html");
         document.getElementById("title").textContent = now;
         document.getElementById("iframe").src = src;
-        document.getElementById("註冊").disabled = (login == 1? false:true);
-        document.getElementById("登入").disabled = (login == 0? false:true);
+        document.getElementById("註冊").disabled = (login == 1 ? false : true);
+        document.getElementById("登入").disabled = (login == 0 ? false : true);
     }
 </script>
-<head><title id = "title">login</title></head>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title id="title">login</title>
+    <style>
+        html {
+            height: 100%;
+        }
+
+        body {
+            margin: 0;
+        }
+
+        .bg {
+            animation: slide 3s ease-in-out infinite alternate;
+            background-image: linear-gradient(-60deg, #e6d6b2 50%, #b07e4a 50%);
+            bottom: 0;
+            left: -50%;
+            opacity: .5;
+            position: fixed;
+            right: -50%;
+            top: 0;
+            z-index: -1;
+        }
+
+        .bg2 {
+            animation-direction: alternate-reverse;
+            animation-duration: 4s;
+        }
+
+        .bg3 {
+            animation-duration: 5s;
+        }
+
+        .content {
+            background-color: rgba(255, 255, 255, .8);
+            border-radius: .25em;
+            box-shadow: 0 0 .25em rgba(0, 0, 0, .25);
+            box-sizing: border-box;
+            left: 50%;
+            padding: 10vmin;
+            position: fixed;
+            text-align: center;
+            top: 50%;
+            transform: translate(-50%, -50%);
+        }
+
+        h1 {
+            font-family: monospace;
+        }
+
+        @keyframes slide {
+            0% {
+                transform: translateX(-25%);
+            }
+
+            100% {
+                transform: translateX(25%);
+            }
+        }
+    </style>
+    <link href="jquery-ui.css" rel="stylesheet" />
+	<script src="external/jquery/jquery.js"></script>
+	<script src="jquery-ui.js"></script>
+</head>
+
 <body>
-    <input type="button" value="註冊" id = "註冊" onClick = "change(0)">
-    <input type="button" value="登入" id = "登入" onClick = "change(1)" disabled><br><br>
-    <iframe id = "iframe"
-        src = "./login.html"
-        height="200" 
-        width="300">
-    </iframe>
+    <div class="bg"></div>
+    <div class="bg bg2"></div>
+    <div class="bg bg3"></div>
+    <div class="content">
+        <input class="btn btn btn-secondary" type="button" value="註冊" id="註冊" onClick="change(0)">
+        <input class="btn btn btn-secondary" type="button" value="登入" id="登入" onClick="change(1)" disabled><br><br>
+        <iframe id="iframe" src="./login.html" height="200" width="300">
+        </iframe>
+    </div>
 </body>
+
 </html>
--- single_info.php
+++ single_info.php
@@ -46,12 +46,161 @@ $cate_val = $category_arr[$cate_id];
         body {
             background-color: white;
         }
+
         a {
-			text-decoration: none;
-			font-family: 微軟正黑體, 新細明體, 標楷體;
-			font-weight: bold;
-			font-size: 17px;
-		}
+            text-decoration: none;
+            font-family: 微軟正黑體, 新細明體, 標楷體;
+            font-weight: bold;
+            font-size: 17px;
+        }
+
+        @import url('https://fonts.googleapis.com/css?family=Exo:400,700');
+
+        * {
+            margin: 0px;
+            padding: 0px;
+        }
+
+        body {
+            font-family: 'Exo', sans-serif;
+        }
+
+
+        .context {
+            width: 100%;
+            position: absolute;
+            top: 50vh;
+
+        }
+
+        .context h1 {
+            text-align: center;
+            color: #fff;
+            font-size: 50px;
+        }
+
+
+        .area {
+            /* background: #4e54c8;
+            background: -webkit-linear-gradient(to left, #8f94fb, #4e54c8); */
+            width: 100%;
+            height: 100%;
+        }
+
+        .circles {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+        }
+
+        .circles li {
+            position: absolute;
+            display: block;
+            list-style: none;
+            width: 20px;
+            height: 20px;
+            background: rgba(118, 65, 10, 0.3);
+            animation: animate 25s linear infinite;
+            bottom: -150px;
+
+        }
+
+        .circles li:nth-child(1) {
+            left: 25%;
+            width: 80px;
+            height: 80px;
+            animation-delay: 0s;
+        }
+
+
+        .circles li:nth-child(2) {
+            left: 10%;
+            width: 20px;
+            height: 20px;
+            animation-delay: 2s;
+            animation-duration: 12s;
+        }
+
+        .circles li:nth-child(3) {
+            left: 70%;
+            width: 20px;
+            height: 20px;
+            animation-delay: 4s;
+        }
+
+        .circles li:nth-child(4) {
+            left: 40%;
+            width: 60px;
+            height: 60px;
+            animation-delay: 0s;
+            animation-duration: 18s;
+        }
+
+        .circles li:nth-child(5) {
+            left: 65%;
+            width: 20px;
+            height: 20px;
+            animation-delay: 0s;
+        }
+
+        .circles li:nth-child(6) {
+            left: 75%;
+            width: 110px;
+            height: 110px;
+            animation-delay: 3s;
+        }
+
+        .circles li:nth-child(7) {
+            left: 35%;
+            width: 150px;
+            height: 150px;
+            animation-delay: 7s;
+        }
+
+        .circles li:nth-child(8) {
+            left: 50%;
+            width: 25px;
+            height: 25px;
+            animation-delay: 15s;
+            animation-duration: 45s;
+        }
+
+        .circles li:nth-child(9) {
+            left: 20%;
+            width: 15px;
+            height: 15px;
+            animation-delay: 2s;
+            animation-duration: 35s;
+        }
+
+        .circles li:nth-child(10) {
+            left: 85%;
+            width: 150px;
+            height: 150px;
+            animation-delay: 0s;
+            animation-duration: 11s;
+        }
+
+
+
+        @keyframes animate {
+
+            0% {
+                transform: translateY(0) rotate(0deg);
+                opacity: 1;
+                border-radius: 0;
+            }
+
+            100% {
+                transform: translateY(-1000px) rotate(720deg);
+                opacity: 0;
+                border-radius: 50%;
+            }
+
+        }
     </style>
     <link href="jquery-ui.css" rel="stylesheet" />
     <script src="external/jquery/jquery.js"></script>
@@ -92,19 +241,19 @@ $cate_val = $category_arr[$cate_id];
                     cat_val = "top";
                 }
                 else if ($("#下著").is(":checked")) {
-                    cat_val ="down";
+                    cat_val = "down";
                 }
                 else if ($("#連身裙").is(":checked")) {
-                    cat_val ="overall";
+                    cat_val = "overall";
                 }
                 if ($("#可愛").is(":checked")) {
                     sty_val = "cute";
                 }
                 else if ($("#簡約").is(":checked")) {
-                    sty_val ="simple";
+                    sty_val = "simple";
                 }
                 else if ($("#優雅").is(":checked")) {
-                    sty_val ="grace";
+                    sty_val = "grace";
                 }
                 console.log(sty_val);
                 $.ajax({
@@ -132,9 +281,10 @@ $cate_val = $category_arr[$cate_id];
 </head>
 
 <body>
-    <div class="container-fluid m-1">
+    <div class="container-fluid m-1" style="z-index: 100;">
         <header class="navbar navbar-expand-sm bd-navbar sticky-top " width="100%" style="background-color: white;">
-            <nav class="row navbar navbar-expand-sm navbar-light d-flex align-items-start" style="background-color: white;">
+            <nav class="row navbar navbar-expand-sm navbar-light d-flex align-items-start"
+                style="background-color: white;">
                 <div class="container-fluid d-flex align-items-start">
                     <div class="row menu ">
                         <div>
@@ -220,7 +370,7 @@ $cate_val = $category_arr[$cate_id];
                         </div>
                     </div>
                 </div>
-                <div class="row  d-flex align-items-center m-1 ">
+                <div class="row  d-flex align-items-center m-1 " style="z-index: 1000;">
                     <div class="row ">
                         <div class="col-6 col-sm-6 d-flex align-items-center">
                             <a href="single.php"><button type="button" class="btn btn-secondary">離開</button></a>
@@ -409,6 +559,20 @@ $cate_val = $category_arr[$cate_id];
             </div>
         </div>
     </div>
+    <div class="area" style="position: absolute; top:0px;">
+            <ul class="circles">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
 </body>
 
 </html>
